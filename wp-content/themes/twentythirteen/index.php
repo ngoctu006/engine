a08df5a0adcf944040f0cb38dbe8813ea61f375b
ngoctu006 <tuit006@gmail.com>
2015-07-23 00:05:14 +0700

    upload source

---
+++ wp-content/themes/twentythirteen/index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme and one of the
+ * two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * For example, it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+                    <h1 class="description">L’Atelier de l’Art et du Média</h1>
+                    <div class="wrap-logo">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images-css/large-logo.png">
+                    </div>
+                    <div class="creatives">
+                        <div class="smoke"></div>
+                    </div>
+		</div><!-- #content -->
+	</div><!-- #primary -->
+<?php get_footer(); ?>
